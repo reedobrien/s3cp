2ad850d925069482a7c8d619dc1d90d81ab50a14
Reed O'Brien <reed@reedobrien.com>
2018-01-30 15:50:03 -0800

    copier copy port with testing part 1
    Adds tests and removes object interface dependency while embedding the
    full CopyObjectInput so we can set any flags/settings that we want in a
    copy.
    
    - Add singlePartcopyObject and tests
    - Fix import and update deps for merge
    - Flesh out tests for getContentLength & objectInfo
    - Use CopyOjectWithContext and embed CopyObjectInput
    
    We use the WithContextMethods so we can cancel in flight items if we
    exit early, e.g. on a signal. Also we want to be able to set any s3
    options on copy so embed the full s3.CopyObjectInput struct.

--- lib/copy_private_test.go
+++ lib/copy_private_test.go
@@ -1,11 +1,124 @@
 package s3cp
 
 import (
+	"errors"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/reedobrien/checkers"
 )
 
-func TestRemovemePrivate(t *testing.T) {
-	checkers.Equals(t, removeMe(), true)
+func TestSetGetErr(t *testing.T) {
+	tut := copier{}
+
+	checkers.OK(t, tut.getErr())
+
+	tut.setErr(errors.New("uh-oh"))
+
+	checkers.Equals(t, tut.getErr().Error(), "uh-oh")
+}
+
+func TestGetContentLengthProvided(t *testing.T) {
+	tut := copier{in: CopyInput{
+		Size: 10,
+	}}
+
+	tut.getContentLength()
+
+	checkers.OK(t, tut.getErr())
+	checkers.Assert(t, tut.contentLength != nil, "got nil, expected value for contentLength")
+	checkers.Equals(t, *tut.contentLength, int64(10))
+}
+
+func TestGetContentLengthLookup(t *testing.T) {
+	api := newDummyAPI(func(d *dummyAPI) {
+		d.Hoo = &s3.HeadObjectOutput{ContentLength: aws.Int64(int64(6))}
+	})
+	tut := copier{
+		in: CopyInput{
+			COI: s3.CopyObjectInput{
+				CopySource: aws.String("bucket/key")},
+		},
+		cfg: Copier{SrcS3: api},
+	}
+
+	tut.getContentLength()
+
+	checkers.OK(t, tut.getErr())
+	checkers.Assert(t, tut.contentLength != nil, "got nil, expected value for contentLength")
+	checkers.Equals(t, *tut.contentLength, int64(6))
+}
+
+func TestGetContentLengthLookupError(t *testing.T) {
+	api := newDummyAPI(func(d *dummyAPI) {
+		d.HooErr = errors.New("boom")
+	})
+	tut := copier{
+		in: CopyInput{
+			COI: s3.CopyObjectInput{
+				CopySource: aws.String("bucket/key")},
+		},
+		cfg: Copier{SrcS3: api},
+	}
+
+	tut.getContentLength()
+
+	checkers.Assert(t, tut.contentLength == nil, "got nil, expected value for contentLength")
+	checkers.Equals(t, tut.getErr().Error(), "error getting object info: boom")
+}
+
+func TestObjectInfoNoCopySource(t *testing.T) {
+	// TODO(ro) 2018-02-01 Can this happen?
+	api := newDummyAPI(func(d *dummyAPI) {
+		d.HooErr = errors.New("boom")
+	})
+	tut := copier{
+		in:  CopyInput{},
+		cfg: Copier{SrcS3: api},
+	}
+
+	tut.getContentLength()
+
+	checkers.Assert(t, tut.contentLength == nil, "got nil, expected value for contentLength")
+	checkers.Equals(t, tut.getErr().Error(), "got nil *string as CopySource")
+}
+
+func newDummyAPI(opts ...func(*dummyAPI)) *dummyAPI {
+	d := &dummyAPI{}
+	for _, opt := range opts {
+		opt(d)
+	}
+	return d
+}
+
+type dummyAPI struct {
+	Coo    *s3.CopyObjectOutput
+	CooErr error
+	Doo    *s3.DeleteObjectOutput
+	DooErr error
+	Hoo    *s3.HeadObjectOutput
+	HooErr error
+}
+
+func (d *dummyAPI) CopyObjectWithContext(_ aws.Context, in *s3.CopyObjectInput, opts ...request.Option) (*s3.CopyObjectOutput, error) {
+	if d.CooErr != nil {
+		return nil, d.CooErr
+	}
+	return d.Coo, nil
+}
+
+func (d *dummyAPI) HeadObjectWithContext(ctx aws.Context, in *s3.HeadObjectInput, opts ...request.Option) (*s3.HeadObjectOutput, error) {
+	if d.HooErr != nil {
+		return nil, d.HooErr
+	}
+	return d.Hoo, nil
+}
+
+func (d *dummyAPI) DeleteObjectWithContext(ctx aws.Context, in *s3.DeleteObjectInput, opts ...request.Option) (*s3.DeleteObjectOutput, error) {
+	if d.DooErr != nil {
+		return nil, d.DooErr
+	}
+	return d.Doo, nil
 }
--- lib/copy_test.go
+++ lib/copy_test.go
@@ -1,10 +1,17 @@
 package s3cp_test
 
 import (
+	"bytes"
+	"errors"
+	"log"
+	"os"
+	"strings"
+	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/s3"
 
@@ -13,7 +20,7 @@ import (
 )
 
 func TestNewDefaults(t *testing.T) {
-	api := newDummyAPI(nil, nil)
+	api := newDummyAPI("")
 	got := s3cp.NewCopier(api)
 
 	checkers.Equals(t, got.PartSize, int64(s3cp.DefaultCopyPartSize))
@@ -23,12 +30,13 @@ func TestNewDefaults(t *testing.T) {
 }
 
 func TestNewWithOptions(t *testing.T) {
-	api := newDummyAPI(nil, nil)
+	api := newDummyAPI("")
 	got := s3cp.NewCopier(api,
 		func(c *s3cp.Copier) { c.PartSize = 100 },
 		func(c *s3cp.Copier) { c.Concurrency = 8 },
 		func(c *s3cp.Copier) { c.Timeout = time.Second },
 		func(c *s3cp.Copier) { c.LeavePartsOnError = true },
+		func(c *s3cp.Copier) { c.MustSvcForRegion = func(s *string) s3cp.API { return newDummyAPI(*s) } },
 	)
 
 	checkers.Equals(t, got.PartSize, int64(100))
@@ -38,7 +46,7 @@ func TestNewWithOptions(t *testing.T) {
 }
 
 func TestWithCopierRequestOptions(t *testing.T) {
-	api := newDummyAPI(nil, nil)
+	api := newDummyAPI("a-region")
 	tut := s3cp.NewCopier(api)
 
 	s3cp.WithCopierRequestOptions(
@@ -49,33 +57,324 @@ func TestWithCopierRequestOptions(t *testing.T) {
 	checkers.Equals(t, len(tut.RequestOptions), 2)
 }
 
-func TestCopy(t *testing.T) {
-	api := newDummyAPI(&s3.CopyObjectOutput{
-		CopyObjectResult: &s3.CopyObjectResult{
-			ETag:         aws.String("etag"),
-			LastModified: aws.Time(time.Date(2005, 7, 1, 9, 30, 00, 00, time.UTC)),
+func TestCopyGetObjectHeadObjectError(t *testing.T) {
+	api := newDummyAPI("", func(d *dummyAPI) {
+		d.Coo = &s3.CopyObjectOutput{
+			CopyObjectResult: &s3.CopyObjectResult{
+				ETag:         aws.String("etag"),
+				LastModified: aws.Time(time.Date(2005, 7, 1, 9, 30, 00, 00, time.UTC)),
+			},
+		}
+	},
+	)
+
+	in := s3cp.CopyInput{
+		COI: s3.CopyObjectInput{
+			CopySource: aws.String("bucket/key"),
 		},
-	}, nil)
+		SourceRegion: aws.String("another-region"),
+	}
 
-	in := s3cp.CopyInput{}
+	tut := s3cp.NewCopier(api,
+		func(c *s3cp.Copier) {
+			c.MustSvcForRegion = func(s *string) s3cp.API {
+				return newDummyAPI(*s, func(d *dummyAPI) {
+					d.Hoo = &s3.HeadObjectOutput{ContentLength: aws.Int64(int64(6))}
+					d.HooErr = errors.New("boom")
+				})
+			}
+		},
+	)
 
-	tut := s3cp.NewCopier(api)
-	err := tut.Copy(in)
+	err := tut.Copy(in, func(c *s3cp.Copier) { c.Concurrency = 1 })
+	checkers.Equals(t, err.Error(), "error getting object info: boom")
+}
+
+func TestCopyDeleteCalled(t *testing.T) {
+	api := newDummyAPI("", func(d *dummyAPI) {
+		d.Coo = &s3.CopyObjectOutput{
+			CopyObjectResult: &s3.CopyObjectResult{
+				ETag:         aws.String("etag"),
+				LastModified: aws.Time(time.Date(2005, 7, 1, 9, 30, 00, 00, time.UTC)),
+			},
+		}
+	},
+	)
+
+	in := s3cp.CopyInput{SourceRegion: aws.String("another-region"),
+		Delete: true,
+		COI: s3.CopyObjectInput{
+			CopySource: aws.String("bucket/key"),
+		},
+	}
+	api2 := newDummyAPI("api2-region", func(d *dummyAPI) {
+		d.Doo = &s3.DeleteObjectOutput{
+			DeleteMarker: aws.Bool(false),
+		}
+		d.Hoo = &s3.HeadObjectOutput{ContentLength: aws.Int64(int64(6))}
+	})
+
+	tut := s3cp.NewCopier(api,
+		func(c *s3cp.Copier) {
+			c.SrcS3 = api2
+			c.MustSvcForRegion = func(_ *string) s3cp.API {
+				return api2
+			}
+		},
+	)
+
+	err := tut.Copy(in, func(c *s3cp.Copier) { c.Concurrency = 1 })
+	checkers.OK(t, err)
+	time.Sleep(time.Second)
+	checkers.Equals(t, api2.DooCalls, int64(1))
+}
+
+func TestCopyDeleteError(t *testing.T) {
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer func() {
+		log.SetOutput(os.Stderr)
+	}()
+	api := newDummyAPI("", func(d *dummyAPI) {
+		d.Coo = &s3.CopyObjectOutput{
+			CopyObjectResult: &s3.CopyObjectResult{
+				ETag:         aws.String("etag"),
+				LastModified: aws.Time(time.Date(2005, 7, 1, 9, 30, 00, 00, time.UTC)),
+			},
+		}
+	},
+	)
+
+	in := s3cp.CopyInput{SourceRegion: aws.String("another-region"),
+		Delete: true,
+		COI: s3.CopyObjectInput{
+			CopySource: aws.String("bucket/key"),
+		},
+	}
+	api2 := newDummyAPI("api2-region", func(d *dummyAPI) {
+		d.DooErr = errors.New("delete boom")
+		d.Doo = &s3.DeleteObjectOutput{
+			DeleteMarker: aws.Bool(false),
+		}
+		d.Hoo = &s3.HeadObjectOutput{ContentLength: aws.Int64(int64(6))}
+	})
+
+	tut := s3cp.NewCopier(api,
+		func(c *s3cp.Copier) {
+			c.SrcS3 = api2
+			c.MustSvcForRegion = func(_ *string) s3cp.API {
+				return api2
+			}
+		},
+	)
+
+	err := tut.Copy(in, func(c *s3cp.Copier) { c.Concurrency = 1 })
+	checkers.OK(t, err)
+	checkers.Equals(t, api2.DooCalls, int64(1))
+	checkers.Assert(t,
+		strings.HasSuffix(
+			buf.String(),
+			"failed to delete \"bucket/key\": \"delete boom\"\n"),
+		"missing expected log message")
+}
+
+func TestCopyDeleteMissingSourceError(t *testing.T) {
+	api := newDummyAPI("", func(d *dummyAPI) {
+		d.Coo = &s3.CopyObjectOutput{
+			CopyObjectResult: &s3.CopyObjectResult{
+				ETag:         aws.String("etag"),
+				LastModified: aws.Time(time.Date(2005, 7, 1, 9, 30, 00, 00, time.UTC)),
+			},
+		}
+	},
+	)
+
+	in := s3cp.CopyInput{SourceRegion: aws.String("another-region"),
+		Size:   68,
+		Delete: true,
+	}
+	api2 := newDummyAPI("api2-region", func(d *dummyAPI) {
+		d.DooErr = errors.New("delete boom")
+		d.Doo = &s3.DeleteObjectOutput{
+			DeleteMarker: aws.Bool(false),
+		}
+		d.Hoo = &s3.HeadObjectOutput{ContentLength: aws.Int64(int64(6))}
+	})
+
+	tut := s3cp.NewCopier(api,
+		func(c *s3cp.Copier) {
+			c.SrcS3 = api2
+			c.MustSvcForRegion = func(_ *string) s3cp.API {
+				return api2
+			}
+		},
+	)
+
+	err := tut.Copy(in, func(c *s3cp.Copier) { c.Concurrency = 1 })
+	checkers.OK(t, err)
+	checkers.Equals(t, api2.DooCalls, int64(0))
+}
+
+func TestSinglePartCopy(t *testing.T) {
+	api := newDummyAPI("", func(d *dummyAPI) {
+		d.Coo = &s3.CopyObjectOutput{
+			CopyObjectResult: &s3.CopyObjectResult{
+				ETag:         aws.String("etag"),
+				LastModified: aws.Time(time.Date(2005, 7, 1, 9, 30, 00, 00, time.UTC)),
+			},
+		}
+	},
+	)
+
+	in := s3cp.CopyInput{SourceRegion: aws.String("another-region"),
+		COI: s3.CopyObjectInput{
+			CopySource: aws.String("bucket/key"),
+		},
+	}
+
+	tut := s3cp.NewCopier(api,
+		func(c *s3cp.Copier) {
+			c.MustSvcForRegion = func(s *string) s3cp.API {
+				return newDummyAPI(*s, func(d *dummyAPI) {
+					d.Hoo = &s3.HeadObjectOutput{ContentLength: aws.Int64(int64(6))}
+				})
+			}
+		},
+	)
+
+	err := tut.Copy(in, func(c *s3cp.Copier) { c.Concurrency = 1 })
 	checkers.OK(t, err)
 }
 
-func newDummyAPI(coo *s3.CopyObjectOutput, err error) *dummyAPI {
-	return &dummyAPI{err: err, coo: coo}
+func TestSinglePartCopyAWSErr(t *testing.T) {
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer func() {
+		log.SetOutput(os.Stderr)
+	}()
+	api := newDummyAPI("", func(d *dummyAPI) {
+		d.Coo = &s3.CopyObjectOutput{
+			CopyObjectResult: &s3.CopyObjectResult{
+				ETag:         aws.String("etag"),
+				LastModified: aws.Time(time.Date(2005, 7, 1, 9, 30, 00, 00, time.UTC)),
+			},
+		}
+		d.CooErr = awserr.New("boomCode", "boomMsg", errors.New("boom"))
+	},
+	)
+
+	in := s3cp.CopyInput{SourceRegion: aws.String("another-region"),
+		COI: s3.CopyObjectInput{
+			CopySource: aws.String("bucket/key"),
+			Key:        aws.String("akey"),
+			Bucket:     aws.String("abucket"),
+		},
+	}
+
+	tut := s3cp.NewCopier(api,
+		func(c *s3cp.Copier) {
+			c.MustSvcForRegion = func(s *string) s3cp.API {
+				return newDummyAPI(*s, func(d *dummyAPI) {
+					d.Hoo = &s3.HeadObjectOutput{ContentLength: aws.Int64(int64(6))}
+				})
+			}
+		},
+	)
+
+	err := tut.Copy(in, func(c *s3cp.Copier) { c.Concurrency = 1 })
+	checkers.Equals(t, err.Error(), "boomCode: boomMsg\ncaused by: boom")
+	checkers.Assert(t, strings.HasSuffix(buf.String(), "failed to copy \"bucket/key\" to \"abucket/akey\": boomCode: boomMsg\ncaused by: boom\n"), "missing expected log message")
+}
+
+func TestSinglePartCopyError(t *testing.T) {
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer func() {
+		log.SetOutput(os.Stderr)
+	}()
+	api := newDummyAPI("", func(d *dummyAPI) {
+		d.Coo = &s3.CopyObjectOutput{
+			CopyObjectResult: &s3.CopyObjectResult{
+				ETag:         aws.String("etag"),
+				LastModified: aws.Time(time.Date(2005, 7, 1, 9, 30, 00, 00, time.UTC)),
+			},
+		}
+		d.CooErr = errors.New("boom")
+	},
+	)
+
+	in := s3cp.CopyInput{SourceRegion: aws.String("another-region"),
+		COI: s3.CopyObjectInput{
+			CopySource: aws.String("bucket/key"),
+			Key:        aws.String("akey"),
+			Bucket:     aws.String("abucket"),
+		},
+	}
+
+	tut := s3cp.NewCopier(api,
+		func(c *s3cp.Copier) {
+			c.MustSvcForRegion = func(s *string) s3cp.API {
+				return newDummyAPI(*s, func(d *dummyAPI) {
+					d.Hoo = &s3.HeadObjectOutput{ContentLength: aws.Int64(int64(6))}
+				})
+			}
+		},
+	)
+
+	err := tut.Copy(in, func(c *s3cp.Copier) { c.Concurrency = 1 })
+	checkers.Equals(t, err.Error(), "boom")
+	checkers.Assert(t, strings.HasSuffix(buf.String(), "failed to copy \"bucket/key\" to \"abucket/akey\": boom\n"), "missing expected log message")
+}
+func newDummyAPI(region string, opts ...func(*dummyAPI)) *dummyAPI {
+	if region == "" {
+		region = "default-region"
+	}
+
+	d := &dummyAPI{region: aws.String(region)}
+
+	for _, opt := range opts {
+		opt(d)
+	}
+	return d
 }
 
 type dummyAPI struct {
-	err error
-	coo *s3.CopyObjectOutput
+	region *string
+
+	CooErr   error
+	Coo      *s3.CopyObjectOutput
+	Hoo      *s3.HeadObjectOutput
+	HooErr   error
+	Doo      *s3.DeleteObjectOutput
+	DooCalls int64
+	DooErr   error
 }
 
-func (d *dummyAPI) CopyObject(in *s3.CopyObjectInput) (*s3.CopyObjectOutput, error) {
-	if d.err != nil {
-		return nil, d.err
+func (d *dummyAPI) CopyObjectWithContext(_ aws.Context, in *s3.CopyObjectInput, opts ...request.Option) (*s3.CopyObjectOutput, error) {
+	if d.CooErr != nil {
+		return nil, d.CooErr
 	}
-	return d.coo, nil
+	return d.Coo, nil
+}
+
+func (d *dummyAPI) HeadObjectWithContext(ctx aws.Context, in *s3.HeadObjectInput, opts ...request.Option) (*s3.HeadObjectOutput, error) {
+	if d.HooErr != nil {
+		return nil, d.HooErr
+	}
+	return d.Hoo, nil
+}
+
+func (d *dummyAPI) DeleteObjectWithContext(ctx aws.Context, in *s3.DeleteObjectInput, opts ...request.Option) (*s3.DeleteObjectOutput, error) {
+	_ = atomic.AddInt64(&d.DooCalls, 1)
+	if d.DooErr != nil {
+		return nil, d.DooErr
+	}
+	return d.Doo, nil
+}
+
+func (d *dummyAPI) Region() string {
+	if d.region == nil {
+		return ""
+	}
+	return *d.region
 }
